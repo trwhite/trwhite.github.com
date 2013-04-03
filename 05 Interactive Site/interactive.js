e7a2b116cd0f406171225a7c0064e27521ad60c9
Thomas White <twhite3792@comcast.net>
2013-04-02 23:18:05 -0400

    IT WORKS MOTHER F*CKER!

--- 05 Interactive Site/interactive.js
+++ 05 Interactive Site/interactive.js
@@ -1,5 +1,33 @@
+$(function() {
+
 $("#checkbox-one").hover(function() { 
    $("#pieone-blue").css("zIndex",100); 
 }, function() { 
    $("#pieone-blue").css("zIndex",0) 
+});
+
+$("#checkbox-two").hover(function() { 
+   $("#pietwo-blue").css("zIndex",100); 
+}, function() { 
+   $("#pietwo-blue").css("zIndex",0) 
+});
+
+$("#checkbox-three").hover(function() { 
+   $("#piethree-blue").css("zIndex",100); 
+}, function() { 
+   $("#piethree-blue").css("zIndex",0) 
+});
+
+$("#checkbox-four").hover(function() { 
+   $("#piefour-blue").css("zIndex",100); 
+}, function() { 
+   $("#piefour-blue").css("zIndex",0) 
+});
+
+$("#checkbox-five").hover(function() { 
+   $("#piefive-blue").css("zIndex",100); 
+}, function() { 
+   $("#piefive-blue").css("zIndex",0) 
+});
+
 });
