95932b9cdb9c6828986e619292728a16244a29f6
Thomas White <twhite3792@comcast.net>
2013-04-16 21:14:04 -0400

    Crime Bar Chart.

---
+++ 06 Crime Rates/datavis2.js
@@ -0,0 +1,53 @@
+$(function() {
+
+	var myParagraph = $("<p>").text("Boston Crime Rates")
+							  .appendTo("body");
+
+});
+
+$(function() {
+
+	var i = 0;
+    while(i < data.length) {
+
+  
+	    var area = data[i];
+		var neighborhoods = area.name;
+		var incidents = parseInt(area.incidents);
+		var domestic = parseInt(area.domestic);
+		
+	
+
+	var maxIncidents = 10651;
+	var fraction = incidents / maxIncidents;
+	var incidentWidth = fraction * 1000;
+	
+	var maxDomestic = 2840;
+	var fraction = domestic / maxDomestic;
+	var domesticWidth = fraction * 500;
+	
+	var row = $("<div>").addClass("row");
+	
+	var neighborhoodDiv = $("<div>").addClass("neighborhood")
+					.text(neighborhoods)
+					.appendTo(row);
+					
+	var incidentDiv = $("<div>")
+					.text(incidents).addClass("incidents")
+					.appendTo(row)
+					.css({ width:incidentWidth,
+        			backgroundColor: "pink" }); 
+        			
+    var domesticDiv = $("<div>")
+					.text(domestic).addClass("domestic")
+					.appendTo(row)
+					.css({ width:domesticWidth,
+        			backgroundColor: "light-grey" }); 
+
+
+    $("body").append(row);        
+    i++;
+    
+    
+   }
+});
